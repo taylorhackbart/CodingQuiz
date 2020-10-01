65eec43760c229998226f15aee797d148584ff6a
Taylor <taylor.hackbart@du.edu>
2020-09-30 19:32:57 -0600

    I CANT BELIEVE IT

--- script.js
+++ script.js
@@ -1,128 +1,179 @@
 // variables
+var highscore = document.getElementById("high-scores"); //high score link
 var start = document.getElementById("start"); //only the button
 var displayCount = document.querySelector(".countdown"); // showing countdown
-var container = document.querySelector(".container"); // header
-var quizStart = document.querySelector(".questions"); // button types
-var button1 = document.getElementById("button1"); // question button A
-var button2 = document.getElementById("button2"); // question button A
-var button3 = document.getElementById("button3"); // question button A
-var button4 = document.getElementById("button4"); // question button A
-var title = document.getElementById("title"); //questions header title
+var container = document.querySelector(".header"); // header
+var quizSection = document.querySelector(".quiz-elements"); //quiz body
+var questionId = document.getElementById("question"); //questions
+var answerId = document.getElementById("answer"); // answer
+var messageId = document.getElementById("message"); //correct or incorrect
+var resultsId = document.querySelector("#results"); // results section
+var resultsInitials = document.getElementById("initials");
+var resultsScore = document.getElementById("score");
+var currentScore = document.getElementById("current-score");
+var userName = "";
 var timeLeft = 60;
 var score = 0;
 var questionIndex = 0;
-
+var letter = ["A) ", "B) ", "C) ", "D) "];
+var leaderboard = [];
 //questions array
 var questionsArr = [
-  {
+    {
     question: " Where does the script.js link go in your html doc? ",
-    choices: ["in the head","it doesn't go in the html doc", "after closing body tag","above closing body tag",],
+    choices: ["in the head","it doesn't go in the html doc", "after closing body tag","above closing body tag"],
     answer: "above closing body tag",
-  },
-  {
+    },
+    {
     question: " Where do we push our homeworks to (not submitting)? ",
-    choices: [" Gitlab "," GitHub"," Bootstrap"," They magically appear in a webpage!",],
-    answer: "Github",
-  },
-  {
+    choices: [" Gitlab ", "GitHub", "Bootstrap", "They magically appear in a webpage!"],
+    answer: "GitHub",
+    },
+    {
     question: " Which is used to make a list? ",
     choices: ["array", "string", "boolean", "function"],
     answer: "array",
-  },
-  {
+    },
+    {
     question: " How to customize and add styles to your webpage?",
     choices: ["HTML", "CSS", "JavaScript", "pray"],
     answer: "CSS",
-  },
+    },
 ];
 
-// start button
+// Start button
 
-function startQuiz() {
-  container.style.display = "none"; // hiding the button
-  quizStart.style.display = "block";
+function startQuiz(e) {
+  e.preventDefault(e);
+  container.style.display = "none"; // hiding the start button
+  quizSection.style.display = "block"; // showing quiz
   startCountdown();
-} 
+  startingQuestions();
+}
 
-//timer function
+//Timer function
 function startCountdown() {
   var countdown = setInterval(function () {
     timeLeft--;
     displayCount.textContent = timeLeft + " seconds left";
-    if (timeLeft === 0 || displayQuestion === 4) {
-        clearInterval(countdown);
+
+    if (timeLeft === 0 || questionIndex === 4) {
+      clearInterval(countdown);
+      timeLeft.textContent = "Finished!";
+      scoreFinal();
     }
-  
-}, 1000);
-//ask first question
-displayQuestion();
+  }, 1000);
 }
-// adding an click for start button
+
+function startingQuestions() {
+  questionId.textContent = "Question: " + questionsArr[questionIndex].question;
+  startingChoices(questionIndex);
+}
+
+function startingChoices(questionIndex) {
+  //loop through the questions
+  for (var i = 0; i < questionsArr[questionIndex].choices.length; i++) {
+    if (
+      questionId.childElementCount < questionsArr[questionIndex].choices.length
+    ) {
+      //creating buttons for choices
+      var newButton = document.createElement("button");
+      questionId.appendChild(newButton);
+      //displaying the choices with the new button
+      newButton.textContent =
+        letter[i] + " " + questionsArr[questionIndex].choices[i];
+      newButton.setAttribute("class", "button" + [i]);
+      newButton.setAttribute(
+        "data-answer",
+        questionsArr[questionIndex].choices[i]
+      );
+    } else {
+      var existingButton = document.querySelector(".button" + [i]);
+      existingButton.textContent =
+        letter[i] + " " + questionsArr[questionIndex].choices[i];
+      existingButton.setAttribute(
+        "data-answer",
+        questionsArr[questionIndex].choices[i]
+      );
+    }
+  }
+}
+
+//Checks answers for correct/incorrect
+function checkAnswer(e) {
+  e.preventDefault();
+  var userAnswer = e.target.getAttribute("data-answer");
+
+  if (questionsArr[questionIndex].answer == userAnswer) {
+    message.textContent = "Correct!";
+    score++;
+  } else {
+    message.textContent = "Incorrect!";
+    timeLeft -= 5;
+  }
+  if (userAnswer) {
+    questionIndex++;
+    currentScore.textContent = "Current score: " + score;
+    startingQuestions();
+  }
+}
+
+function scoreFinal() {
+  questionId.style.display = "none";
+  var initials = prompt("Enter your initials for the leaderboard.");
+  displayCount.style.display = "none";
+  messageId.style.display = "none";
+  currentScore.style.display = "none";
+  container.textContent = initials + " " + score;
+  resultsId.style.display = "block";
+
+  //adding scores into local storage
+  var previousPlayers = JSON.parse(localStorage.getItem("players"));
+  var currentPlayer = {
+    player: initials,
+    playerScore: score,
+  };
+  //creating a leaderboard
+  leaderboard.push(currentPlayer);
+  if (previousPlayers != null) {
+    for (var l = 0; l < previousPlayers.length; l++) {
+      leaderboard.push(previousPlayers[l]);
+    }
+  } else {
+    var first = document.createElement("h4");
+    first.classList.add("col-md-6");
+    first.classList.add("yourefirst");
+    first.textContent = "You're First To Play!";
+    questionId.insertAdjacentElement("afterend", first);
+  }
+
+  localStorage.setItem("players", JSON.stringify(leaderboard));
+  //high score calculator
+  leaderboard.sort((a, b) => (a.playerScore < b.playerScore ? 1 : -1));
+  //creating list elements to display initials and score
+  var ul = document.createElement("ul");
+  ul.classList.add("col-md-6");
+  ul.classList.add("topThree");
+  var liOne = document.createElement("li");
+  liOne.textContent =
+    leaderboard[0]["player"] + " " + leaderboard[0]["playerScore"];
+  var liTwo = document.createElement("li");
+  liTwo.textContent =
+    leaderboard[1]["player"] + " " + leaderboard[1]["playerScore"];
+  ul.appendChild(liOne);
+  ul.appendChild(liTwo);
+  if (leaderboard[2] != null) {
+    var liThree = document.createElement("li");
+    liThree.textContent =
+      leaderboard[2]["player"] + " " + leaderboard[2]["playerScore"];
+    ul.appendChild(liThree);
+  }
+
+  questionId.insertAdjacentElement("afterend", ul);
+}
+
+//Starts by clicking "Start Quiz"
 start.addEventListener("click", startQuiz);
 
-//adding a click for questions
-button1.addEventListener("click", function() {
-    questionIndex++;
-    displayQuestion();
-});
-button2.addEventListener("click", function() {
-    questionIndex++;
-    displayQuestion();
-});
-button3.addEventListener("click", function() {
-    questionIndex++;
-    displayQuestion();
-});
-button4.addEventListener("click", function() {
-    questionIndex++;
-    displayQuestion();
-});
-
-//first question
-function displayQuestion() {
-    document.querySelector(".title").innerHTML =
-    questionsArr[questionIndex];
-
-    button1.innerHTML =
-    questionsArr[questionIndex].choices[0];
-
-    button2.innerHTML =
-    questionsArr[questionIndex].choices[1];
-
-    button3.innerHTML =
-    questionsArr[questionIndex].choices[2];
-
-    button4.innerHTML =
-    questionsArr[questionIndex].choices[3];
-    
-} end();
-//displaying answer
-var newTime = timeLeft-=15;
-function correct(){
-    if (questionIndex[0] === questionsArr[questionIndex].choices[3]){
-         score++;
-         console.log(score);
-    }  else {
-       newTime; 
-    }}
-   correct();
-
-
-// or incorrect subtract time
-
-
-// all q answered or time = 0, game over
-
-// prompt to initial and score
-
-// if the time runs out, or you've answered all the questions, game over
-function end(){
-if (timeLeft === 0 || displayQuestion === questionsArr[3]) {
-  clearInterval(countdown);
-  // enter initials for scoreboard
-  prompt("Enter initials to be added to the scoreboard!");
-
-  // add up their score
-  alert(score);
-}}
-
+//Checks the answers when a user clicks
+questionId.addEventListener("click", checkAnswer);
